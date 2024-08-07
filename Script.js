1ed4ed7c6e7564b22a085bc796efb3fb2c7cb910
Name <91964534+a-usern4me@users.noreply.github.com>
2024-08-07 15:27:40 +0100

    Adjustments and Refining

--- Script.js
+++
@@ -1,6 +0,0 @@
-let topButton = document.getElementById('btn-back');
-
-function toTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
