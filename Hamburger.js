7832cb0329da796d673a888a6ffbfd7379e79b89
Name <91964534+a-usern4me@users.noreply.github.com>
2026-06-21 15:20:36 +0100

    Refinements

--- Hamburger.js
+++
@@ -1,8 +0,0 @@
-function hamburgerMenu() {
-  var x = document.getElementById("navID");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
